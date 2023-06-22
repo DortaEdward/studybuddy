648b03f336451088b872943db61822a9274bb4a5
Edward Dorta <dortaedward@gmail.com>
2023-06-21 20:08:16 -0400

    initial set up

---
+++ electron/preload.ts
@@ -0,0 +1,33 @@
+function domReady(condition: DocumentReadyState[] = ['complete', 'interactive']) {
+  return new Promise(resolve => {
+    if (condition.includes(document.readyState)) {
+      resolve(true)
+    } else {
+      document.addEventListener('readystatechange', () => {
+        if (condition.includes(document.readyState)) {
+          resolve(true)
+        }
+      })
+    }
+  })
+}
+
+const safeDOM = {
+  append(parent: HTMLElement, child: HTMLElement) {
+    if (!Array.from(parent.children).find(e => e === child)) {
+      parent.appendChild(child)
+    }
+  },
+  remove(parent: HTMLElement, child: HTMLElement) {
+    if (Array.from(parent.children).find(e => e === child)) {
+      parent.removeChild(child)
+    }
+  },
+}
+
+/**
+ * https://tobiasahlin.com/spinkit
+ * https://connoratherton.com/loaders
+ * https://projects.lukehaas.me/css-loaders
+ * https://matejkustec.github.io/SpinThatShit
+ */
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+    <p className="text-3xl">This is using tailwind</p>
+      Hello World
+    </>
+  )
+}
+
+export default App
