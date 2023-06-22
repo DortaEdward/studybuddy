a03acdc0be4817983f76638c26cece1f3c1a28a4
dortaedward <dortaedward@gmail.com>
2023-06-21 20:45:21 -0400

    Started working on the card component

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ function App() {
 
   return (
     <>
-    <p className="text-3xl">This is using tailwind</p>
-      Hello World
+      <p className="text-3xl">This is using tailwind</p>
+      <div className="w-[480px] h-[360px] bg-neutral-800 rounded text-gray-200 flex flex-col items-center justify-between py-4">
+        <div className="flex flex-col items-center gap-1 justify-center h-[150px]">
+          <div id="heading" className="text-lg">Subject</div>
+          <div id="question" className="text-2xl">Is this a question?</div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <textarea className="w-[360px] h-[90px] p-2 resize-none rounded outline-none" placeholder="Enter Answer" />
+          <button className="rounded py-2 bg-blue-500">Submit</button>
+        </div>
+      </div>
     </>
   )
 }
